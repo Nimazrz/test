2d68e70d96846a9475ad7fdaee19751a255b23a3
Nimazrz <Nimaaa8413@gmail.com>
2024-11-18 14:00:54 +0330

    first comit

--- shopproj/settings.py
+++ shopproj/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
     'account.apps.AccountConfig',
+    "cart.apps.CartConfig",
+    "orders.apps.OrdersConfig",
 ]
 
 MIDDLEWARE = [
